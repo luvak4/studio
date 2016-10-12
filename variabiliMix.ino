cf0fbb20ef9a1aec322734528ccc4076e7448070
luvak4 <luvak4@gmail.com>
2016-10-12 13:14:43 +0200

    Update variabiliMix.ino

--- variabiliMix.ino
+++ variabiliMix.ino
@@ -47,31 +47,27 @@ void setup() {
 void decodeMessage(){
   // from byte to struct
   int tempint;
-  BYTE[0]=BYTE[0]^CIFR[0];
+  
+  // de-cifratura
+  cipher();
+  //
   MSGindirizzo=BYTE[0];
   //
-  BYTE[2]=BYTE[2]^CIFR[2];
   tempint=BYTE[2];
   tempint=tempint << 8;
-  BYTE[1]=BYTE[1]^CIFR[1];
   tempint=tempint+BYTE[1];
   MSGvaloreA=tempint;
   //
-  BYTE[4]=BYTE[4]^CIFR[4];
   tempint=BYTE[4];
   tempint=tempint << 8;
-  BYTE[3]=BYTE[3]^CIFR[3];
   tempint=tempint+BYTE[3];
   MSGvaloreB=tempint;
   //
-  BYTE[6]=BYTE[6]^CIFR[6];
   tempint=BYTE[6];
   tempint=tempint << 8;
-  BYTE[5]=BYTE[5]^CIFR[5];
   tempint=tempint+BYTE[5];
   MSGvaloreC=tempint;
   //
-  BYTE[7]=BYTE[7]^CIFR[7];
   MSGvaloreD=BYTE[7];
 }
 
@@ -81,34 +77,34 @@ void encodeMessage(){
   unsigned long mask=0x0000FFFF;
   ////////
   BYTE[0]=MSGindirizzo;
-  BYTE[0]=BYTE[0]^CIFR[0];
   ////////
   intt=MSGvaloreA;
   BYTE[1]=intt & mask;
-  BYTE[1]=BYTE[1]^CIFR[1];
   intt = intt >> 8;
   BYTE[2]=intt & mask;
-  BYTE[2]=BYTE[2]^CIFR[2];
   ////////
   intt=MSGvaloreB;
   BYTE[3]=intt & mask;
-  BYTE[3]=BYTE[3]^CIFR[3];
   intt = intt >> 8;
   BYTE[4]=intt & mask;
-  BYTE[4]=BYTE[4]^CIFR[4];
   ////////
   intt=MSGvaloreC;
   BYTE[5]=intt & mask;
-  BYTE[5]=BYTE[5]^CIFR[5];
   intt = intt >> 8;
   BYTE[6]=intt & mask;
-  BYTE[6]=BYTE[6]^CIFR[6];
   ////////
   BYTE[7]=MSGvaloreD;  
-  BYTE[7]=BYTE[7]^CIFR[7];
+  // cifratura
+  cipher();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
+}
+
+void cipher(){
+  for (byte n=0;n<8;n++){
+    BYTE[n]=BYTE[n]^CIFR[n];
+  }
 }
